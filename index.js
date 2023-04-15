227b27e01ec4ec5764557378987e85cc732a4e37
Sunny Kumar <55615100+sunny8080@users.noreply.github.com>
2023-04-15 20:35:39 +0530

    add all files

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const format = require("date-format");
 
 const app = express();
 
-const PORT = 4000 || process.env.PORT;
+// const PORT = 4000 || process.env.PORT;
+const PORT = process.env.PORT || 4000;
 
 app.get("/", (request, response) => {
     // response.send('<h1>Hello Sunny</h1>')
